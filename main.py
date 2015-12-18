646806395153c98804e2a218876557b4d9a76d24
sgoblin <rsuper@sonic.net>
2015-12-18 06:47:35 +0000

    Added HAL code, and added more acceptable origins.

--- main.py
+++ main.py
@@ -29,6 +29,9 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("You should not be here.")
 
+'''class GetMessagesHandler():
+    '''
+
 class ChatSocketHandler(tornado.websocket.WebSocketHandler):
     handlers = set()
     cache = []
@@ -37,7 +40,7 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
         parsed_origin = urllib.parse.urlparse(origin)
         print(parsed_origin.netloc)
-        return parsed_origin.netloc == "axiom-halt.codio.io" or parsed_origin.netloc == "chatbot.sgoblin.com"
+        return parsed_origin.netloc == "axiom-halt.codio.io" or parsed_origin.netloc == "chatbot.sgoblin.com" or parsed_origin.netloc == "network-limit.codio.io"
         
     @classmethod
     def update_cache(cls, chat):
@@ -71,12 +74,19 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
         print("Done sending previous messages")
             
     def on_message(self, message):
+        imsorrydave = "HAL: I'm sorry Dave, I can't do that."
         print(message)
+        messageData = message.split(":", 1)[1].strip().lower()
         ChatSocketHandler.update_cache(message)
         ChatSocketHandler.send_updates(message)
         chatMessages = self.settings["db"].chatmessages
         messageObject = {"_id": time.time(), "message": message}
         chatMessages.insert(messageObject)
+        if (messageData == "open the pod bay doors, hal" or messageData == "open the pod bay doors hal" or  messageData == "open the pod bay doors, hal!" or messageData == "open the pod bay doors, hal." or messageData == "open the pod bay doors hal!" or messageData == "open the pod bay doors hal."):
+            ChatSocketHandler.update_cache(imsorrydave)
+            ChatSocketHandler.send_updates(imsorrydave)
+            imsorryObject = {"_id": time.time(), "message": imsorrydave}
+            chatMessages.insert(imsorryObject)
         
     def on_close(self):
         ChatSocketHandler.handlers.remove(self)
